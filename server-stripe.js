7a92e2ffe2475f80b98d372f86a947c9de0a7ebe
Jonathan Hernandez <jon.hernandez@gmail.com>
2012-08-19 19:57:29 -0400

    Adding in Stripe server file.
    server-stripe.js file will be used to process all stripe-related commands.
    
    Signed-off-by: Jonathan Hernandez <jon.hernandez@gmail.com>

---
+++ server-stripe.js
@@ -0,0 +1,11 @@
+/*IMPORTANT: Make sure you fill in your secret API key into the stripe module.*/
+var stripe = require('stripe')('SECRET_API_KEY_GOES_HERE');
+
+module.exports = function(app){
+    app.get('/stripe',
+        function(req,res)
+        {
+            res.json({}); //Send out blank data for now.
+        }
+    );
+}
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function(req,res)
-{
-    res.sendfile('index.html');
-});
+require('./server-stripe.js')(app);
 
+//Serve out index.html to root and /index.html GET requests
+app.get(['/','/index.html'],
+    function(req,res)
+    {
+        res.sendfile('index.html');
+    }
+);
+
+//Port server is on. Defaulted to 8080.
 app.listen(8080);
